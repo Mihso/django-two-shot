1213dcd27b8a4d7c3f9798ba36c017b549a55ad2
Matthew Oshimo <moshimo90696@gmail.com>
2022-06-15 12:04:00 -0700

    Feature 8 complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from receipts.models import Receipt
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class ReceiptListView(ListView):
+class ReceiptListView(LoginRequiredMixin, ListView):
     model = Receipt
     template_name = ("receipts/list.html")
+    def get_queryset(self):
+        return Receipt.objects.filter(purchaser=self.user)
