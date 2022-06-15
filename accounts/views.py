f69f9aaee6863821d3d46cf43f2a67881cad2457
Matthew Oshimo <moshimo90696@gmail.com>
2022-06-15 16:04:43 -0700

    Feature polish Complete

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth import login
 # Create your views here.
 def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            new_user = User.objects.create_user(username = request.POST.get("username"), password = request.POST.get("password"))
+            new_user = User.objects.create_user(username = request.POST.get("username"), password = request.POST.get("password"),)
             new_user.save()
+            login(request, new_user)
             return redirect("home")
     else:
         form = UserCreationForm()
